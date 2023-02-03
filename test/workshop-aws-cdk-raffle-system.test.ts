ddce8e010c8ee0fc3693d4062ac8781e4931ff4d
Raphael Manke <raphael.manke@codecentric.de>
2023-02-03 09:12:03 +0100

    Initial commit

---
+++ test/workshop-aws-cdk-raffle-system.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WorkshopAwsCdkRaffleSystem from '../lib/workshop-aws-cdk-raffle-system-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/workshop-aws-cdk-raffle-system-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WorkshopAwsCdkRaffleSystem.WorkshopAwsCdkRaffleSystemStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
